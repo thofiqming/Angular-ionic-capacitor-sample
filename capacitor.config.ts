3e9bbb5abc0b57c441dc597b39fadddbe1e95740
thofiq <thofiqming@gmail.com>
2021-08-08 11:54:41 +0800

    angular ionic capacitor integration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thofiq.demo.angularionic',
+  appName: 'angular-ionic-notes',
+  webDir: 'dist/angular-ionic-notes',
+  bundledWebRuntime: false
+};
+
+export default config;
